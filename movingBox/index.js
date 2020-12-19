8f40db0fe546cb063432e17d3ce6548ad46ad10d
aoken7 <aokensabu1@gmail.com>
2020-12-19 19:49:17 +0900

    edit movingbox

--- movingBox/index.js
+++ movingBox/index.js
@@ -17,23 +17,30 @@ function init() {
 
     // カメラを作成 (視野角, 画面のアスペクト比, カメラに映る最短距離, カメラに映る最遠距離)
     const camera = new THREE.PerspectiveCamera(50, width / height, 1, 10000);
-    camera.position.set(0, 0, +1000);
-
+    camera.position.set(0, -700, +150);
+    camera.rotation.x = 1.5;
 
     // 箱を作成
-    const geometry = new THREE.BoxGeometry(100, 100, 100);
-    const material = new THREE.MeshStandardMaterial({
-        color: 0x0000ff
+    const geometry = new THREE.BoxGeometry(110, 110, 110);
+    const edges = new THREE.EdgesGeometry(geometry);
+    const material = new THREE.LineBasicMaterial({
+        color: 0x0000ff,
     });
-    const box = new THREE.Mesh(geometry, material);
-    box.position.set(0,0,0);
+    const box = new THREE.LineSegments(edges, material);
+    box.position.set(0,0,50);
+    box.rotation.y = 10;
+    box.rotation.x = 10;
     scene.add(box);
+
+    const grid = new THREE.GridHelper(10000, 200, 0x444444, 0x444444);
+    grid.rotation.x = Math.PI / -2;
+    scene.add(grid);
 
     // 平行光源
     const directionalLight = new THREE.DirectionalLight(
         0xffffff
     );
-    directionalLight.position.set(1, 1, 1);
+    directionalLight.position.set(1, -1, 1);
     // シーンに追加
     scene.add(directionalLight);
 
@@ -45,14 +52,14 @@ function init() {
 
     function tick() {
         requestAnimationFrame(tick);
-        angleRad += 2 * Math.PI / 180;
+        angleRad += 1 * Math.PI / 180;
         //const t = new Date().getTime() / 500;
         //console.log(Math.cos(Number(time)));
         // 箱を回転させる
-        //box.rotation.x += 0.01;
-        //box.rotation.y += 0.01;
-        box.position.x = (Math.cos(angleRad)*200);
-        box.position.y = (Math.sin(angleRad)*200);
+        box.rotation.x += 0.01;
+        box.rotation.y += 0.01;
+        //box.position.x = (Math.cos(angleRad)*200);
+        //box.position.y = (Math.sin(angleRad)*200);
         //box.position.x += 1;
         //amera.rotation.y += 0.01;
 
